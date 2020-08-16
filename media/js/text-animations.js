b223ca46e31722d6e71af0d8f713db5460744424
Panos Oikonomou <panos.economou.v@gmail.com>
2020-08-16 16:58:31 +0300

    1 pan commit : )

--- media/js/text-animations.js
+++ media/js/text-animations.js
@@ -13,16 +13,44 @@ for (var i=0;i<titles.length;i++){
 // Animate each span with anime
 anime.timeline()
 .add({
-    targets: '.animated-title .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 500 + 30 * i
+    targets: '.animated-title .letter', // All the elements with these classes will be selected
+    translateX: [40,0],                 // Translation
+    translateZ: 0,                  
+    opacity: [0,1],                     // Opacity range
+    easing: "easeOutExpo",              // Animation style
+    duration: 1200,                     // Duration for each letter
+    delay: (el, i) => 500 + 30 * i      // Delay for each leter i 
 });
 
 
 //////////////////////////////////////////////////////////////////////////
 // Subtitle reveal animation /////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
+
+// window.addEventListener('mousemove', event => {
+//     bg = document.getElementsByClassName('night-bg');
+//     if (bg.length>0){
+//         bg = bg[0];
+//     }
+//     else {
+//         return;
+//     }
+
+//     mouseXpercentage = Math.round(event.offsetX / window.innerWidth * 100);
+//     mouseYpercentage = Math.round(event.offsetY / window.innerHeight * 100);
+
+//     bg.style.background = 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, white, #000712)'
+// });
+
+// var toggle_height = 300;
+// var prev = !(window.scrollY >= toggle_height);
+// window.addEventListener('scroll', function() {
+//     // console.log(window.scrollY);
+//     current = window.scrollY >= toggle_height;
+//     if (current == prev){
+//         body = document.getElementsByTagName('body')[0];
+//         body.classList.toggle('night-bg');
+//     }
+//     // console.log(prev,current)
+//     prev = !current;
+// });
